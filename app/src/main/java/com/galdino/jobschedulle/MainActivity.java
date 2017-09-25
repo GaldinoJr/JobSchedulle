97b9f1203e051e2ae985b289086722e522b23f43
GaldinoMadeInWeb <galdino.ouros@madeinweb.com.br>
2017-09-24 22:40:51 -0300

    Exemple finalized

--- app/src/main/java/com/galdino/jobschedulle/MainActivity.java
+++ app/src/main/java/com/galdino/jobschedulle/MainActivity.java
@@ -1,8 +1,15 @@
 package com.galdino.jobschedulle;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
 import android.content.ComponentName;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,12 +17,11 @@ import com.galdino.jobschedulle.domain.MessageEB;
 import com.galdino.jobschedulle.service.JobSchedulerService;
 
 import de.greenrobot.event.EventBus;
-import me.tatarka.support.job.JobInfo;
-import me.tatarka.support.job.JobScheduler;
-import me.tatarka.support.os.PersistableBundle;
 
+//https://developer.android.com/reference/android/app/job/JobInfo.Builder.html
 public class MainActivity extends AppCompatActivity
 {
+    private static final int JOB_ID = 1;
     private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -35,34 +41,47 @@ public class MainActivity extends AppCompatActivity
     }
 
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void onJobExecute(View view)
     {
+        Log.i("LOG_JOB","onJobExecute");
         ComponentName componentName = new ComponentName(this, JobSchedulerService.class);
 
         PersistableBundle pb = new PersistableBundle();
         pb.putString("string","Qualquer coisa lalala");
 
-        JobInfo jobInfo = new JobInfo.Builder(1,componentName)
-                .setBackoffCriteria(4000,JobInfo.BACKOFF_POLICY_LINEAR)
-                .setExtras(pb)
-                //.setMinimumLatency(2000)
-                //.setOverrideDeadline(2000)
-                .setPersisted(true)
-                // Caso mJss.jobFinished(jobParameterses[0],true); for false
-                .setPeriodic(2000)
+        JobInfo.Builder jobInfo =
+                new JobInfo.Builder(JOB_ID, componentName)
+                        // Regra de execução, tempo de looping aparentemente
+                        .setBackoffCriteria(4000, JobInfo.BACKOFF_POLICY_LINEAR)
+
+                        .setPersisted(true) //Defina se deve ou não persistir este trabalho durante as reinicializações do dispositivo.
+
+//                        .setPeriodic(4000)
+
+                .setMinimumLatency(2000) // Latencia mínima de agendamento
+                .setOverrideDeadline(15000) // Latencia máxima de agendamento
+
+                // Precisa de wifi para funcionar
                 .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+
+                // Precisa de qualquer conexão para funcionar
+//                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+
+                .setExtras(pb)
                 // só é ativado quando o celular estiver carregando?
                 .setRequiresCharging(false)
-                .setRequiresDeviceIdle(false)
-                .build();
 
-        JobScheduler jobScheduler = JobScheduler.getInstance(this);
-        jobScheduler.schedule(jobInfo);
+                .setRequiresDeviceIdle(false);
+
+        JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        jobScheduler.schedule(jobInfo.build());
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void onCancelAll(View view){
-        //JobScheduler js = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
-        JobScheduler js = JobScheduler.getInstance(this);
+        JobScheduler js = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+//        JobScheduler js = JobScheduler.getInstance(this);
         js.cancel(1);
     }
 }
--- app/src/main/java/com/galdino/jobschedulle/service/JobSchedulerService.java
+++ app/src/main/java/com/galdino/jobschedulle/service/JobSchedulerService.java
@@ -1,27 +1,45 @@
 package com.galdino.jobschedulle.service;
 
+import android.app.job.JobParameters;
+import android.app.job.JobService;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.SystemClock;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
 
 import com.galdino.jobschedulle.domain.MessageEB;
 import com.galdino.jobschedulle.network.HttpConnection;
 
 import de.greenrobot.event.EventBus;
-import me.tatarka.support.job.JobParameters;
-import me.tatarka.support.job.JobService;
+//import me.tatarka.support.job.JobParameters;
+//import me.tatarka.support.job.JobService;
 
 /**
  * Created by galdino on 24/09/17.
  */
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class JobSchedulerService extends JobService
 {
     private static String LOG = "LOG_JOB";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(LOG, "Service created");
+    }
+
     @Override
     public boolean onStartJob(JobParameters jobParameters)
     {
         Log.i(LOG,"Start Service");
         new MyAsyncTask(this).execute(jobParameters);
+
+        /*
+		 * True - if your service needs to process
+		 * the work (on a separate thread).
+		 * False - if there's no more work to be done for this job.
+		 */
         return true;
     }
 
@@ -40,12 +58,13 @@ public class JobSchedulerService extends JobService
             mJss = jss;
         }
 
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
         @Override
         protected String doInBackground(JobParameters... jobParameterses)
         {
             Log.i(LOG,"doInBackground");
 
-            SystemClock.sleep(5000);
+//            SystemClock.sleep(5000);
 
             String resposta =
                     HttpConnection.getSetDataWeb("http://www.villopim.com.br/android/ExampleJobScheduler/get-random.php",
